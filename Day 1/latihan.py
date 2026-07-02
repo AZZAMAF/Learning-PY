0d964366541d3315bdc354ab9a3fad955c075a91
AZZAMAF <aafazzam@gmail.com>
2026-07-02 19:06:50 +0700

    Day 1 | exam 1

---
+++ Day 1/latihan.py
@@ -0,0 +1,20 @@
+age = 5
+future_age = 10
+result = age + future_age
+
+print(f"In 10 years, I will be {result} years old")
+# kenapa bisa jalan ya tingal umur di tambah umur masa depa lah ez
+
+word = "python"
+print(word[0])
+print(word[5])
+# walau gua tau ini dari ai ya jir logika gua gak masuk soalnya
+# gua udh baca dokumentasi python tapi tetep aja bingung 
+# list itu kek array gitu jirr tapi gua bingug cara implentasiinya untuk 1 kata 
+# padahal kalau di pahami lagi gampang banget ternnyata gak kepikiran
+
+language = ["python", "Java", "C"]
+
+python = language[0]
+print(python[0])
+print(python[5])
