e9359956da3dec890168d3c2d1695cc69e337b14
AZZAMAF <aafazzam@gmail.com>
2026-07-03 21:36:05 +0700

    Day2 | latihan | List and Dictionary

---
+++ Day 2/latihan.py
@@ -0,0 +1,30 @@
+# Problem 2.1: Mengelola Daftar (List)
+#     Buat sebuah list bernama daftar_teman yang berisi 3 nama teman kamu.
+#     Tambahkan satu nama lagi ke daftar tersebut menggunakan fungsi .append().
+#     Print daftar lengkapnya.
+#     Print nama teman yang ada di posisi indeks ke-1 (ingat, Python mulai hitung dari 0).
+
+daftarTeman = [
+    "ken","ihsan","faruf"
+]
+
+print(daftarTeman)
+
+daftarTeman.append("kentaki")
+
+print(daftarTeman)
+
+print(daftarTeman[1])
+
+# Problem 2.2: Kamus Sederhana (Dictionary)
+#     Buat dictionary bernama kontak_teman.
+#     Masukkan 3 teman sebagai Key dan angka (nomor telepon) sebagai Value.
+#     Print nomor telepon salah satu teman dengan cara memanggil namanya (Key-nya).
+
+friendsContact  ={
+    "ken" : 813192012,
+    "san" : 813193212,
+    "ran" : 8123901234,
+}
+
+print(friendsContact["ken"])
